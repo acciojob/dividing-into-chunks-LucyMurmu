958b1c1ae86292c8d2ac96656cb1e801657a915c
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-05-30 02:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	 let result = [];
+    let current = [];
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (sum + arr[i] > n) {
+            result.push(current);
+            current = [arr[i]];
+            sum = arr[i];
+        } else {
+            current.push(arr[i]);
+            sum += arr[i];
+        }
+    }
+
+    if (current.length > 0) {
+        result.push(current);
+    }
+
+    return result;
 };
 
 const n = prompt("Enter n: ");
